0e26949ff111289f3a06b9be18ab78bb75b99ce7
user <evan.n.brown25@gmail.com>
2025-02-22 20:22:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,56 @@
-import json, time;
+import json, time, random;
 from flask import Flask, jsonify, request;
 
-# app = Flask(__name__)
+app = Flask(__name__)
+
+#planning:
+# GETS
+#    get full 8x8 board status
+#    get synchronize request
+#          send: game code, receive: ping, current timer amount
+
+#    get total games in last 24 hours
+#    get amount currently playing
+# POSTS 
+#    post create new game
+#        receive: host player id, game code
+#    post join game
+#        send: game code, receive: confirm, second player id
+#    post next move
+#        send: game code, vector, move type
+
+#get length to find how many games
+startedGames = []
+
+activeGames = {}
+
+def generateCode():
+    code = ""
+    for i in range(4):
+        code += chr(random.randrange(1, 26) + 64)
+    return code
+
+@app.route('/game/create', methods=['POST'])
+def createGame():
+    #generate a random 4 letter code
+    gameCode = generateCode()
+    creationTime = time.time()
+
+    hostID = generateCode()
+    secondPlayerID = generateCode()
+
+    activeGames[gameCode] = {
+        'creationTime': creationTime,
+        'startTime': -1,
+        'hostID': hostID,
+        'secondPlayerID': secondPlayerID
+    }
+
+@app.route('/game/join', methods=['POST'])
+#when join game is called, start game countdown
+def joinGame(gameCode):
+    activeGames[gameCode]
+
 
 # snippets = {1: {'timestamp': -1, 'paste': 'example'}}
 
@@ -29,4 +78,3 @@ from flask import Flask, jsonify, request;
 #     response = {'id': id, **snippets[nextSnippetId]}
 
 #     return jsonify(response), 201
-
